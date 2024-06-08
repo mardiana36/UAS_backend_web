02240e6283b4bd00940a9c8f2fcb249dc5336724
mardiana36 <kelikimardiana@gmail.com>
2024-06-08 19:22:42 +0800

    Penambahan Componets dan assets

---
+++ uas/app/views/components/headers.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $_SESSION['page'] ?></title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="app/views/assets/images/favicon.png">
+    <!-- Custom Stylesheet -->
+    <link href="app/views/assets/plugins/jquery-steps/css/jquery.steps.css" rel="stylesheet">
+    <link href="app/views/assets/css/style.css" rel="stylesheet">
+
+    <link href="app/views/assets/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet">
+    <!-- Page plugins css -->
+    <link href="app/views/assets/plugins/clockpicker/dist/jquery-clockpicker.min.css" rel="stylesheet">
+    <!-- Color picker plugins css -->
+    <link href="app/views/assets/plugins/jquery-asColorPicker-master/css/asColorPicker.css" rel="stylesheet">
+    <!-- Date picker plugins css -->
+    <link href="app/views/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css" rel="stylesheet">
+    <!-- Daterange picker plugins css -->
+    <link href="app/views/assets/plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet">
+    <link href="app/views/assets/plugins/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <style>
+        
+    </style>
+</head>
+
+<body>
