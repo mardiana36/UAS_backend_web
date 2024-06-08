234e8233d1aaafd2a16f139a83a9c3c133dd2e42
triana <agungtrianaputra22@gmail.com>
2024-06-08 23:06:20 +0800

    Update

---
+++ uas/app/views/pembayaran/update.php
@@ -0,0 +1,81 @@
+<!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+            <div class="row page-titles mx-0">
+                <div class="col p-md-0">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
+                    </ol>
+                </div>
+            </div>
+            <!-- row -->
+
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                              <h4 class="card-title">Update Payment</h4>
+                                <div class="form-validation">
+                                    <form class="" action="index.php?action=uPembayaran" method="post">
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="nomor">Room Number<span class="text-danger">*</span>
+                                            </label>
+                                            <div class="col-lg-6">
+                                                <input type="text" class="form-control" id="nomor" name="nomor" placeholder="Input The Room Number..." required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="reservationCode">Room Tipe<span class="text-danger">*</span>
+                                            </label>
+                                            <div class="col-lg-6">
+                                                <select class="form-control" id="pemesanan_id" name="pemesanan_id" required>
+                                                    <option value="" hidden>Pilih...</option>
+                                                    <option value="">Reservation Code</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="metodePembayaran">Payment Method<span class="text-danger">*</span>
+                                            </label>
+                                            <div class="col-lg-6">
+                                                <select class="form-control" id="metodePembayaran" name="metodePembayaran" required>
+                                                    <option value="" hidden>Pilih...</option>
+                                                    <option value="CC">Credit Card</option>
+                                                    <option value="BT">Bank Transfer</option>
+                                                    <option value="CS">Cash</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="status">Status<span class="text-danger">*</span>
+                                            </label>
+                                            <div class="col-lg-6">
+                                                <select class="form-control" id="status" name="status" required>
+                                                    <option value="" hidden>Pilih...</option>
+                                                    <option value="PA">Paid</option>
+                                                    <option value="PE">Pending</option>
+                                                    <option value="FA">Failed</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-lg-8 ml-auto">
+                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
