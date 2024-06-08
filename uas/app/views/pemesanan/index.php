afa0a2926f9b8dbfd571c2675e7f9662341a6cfa
mardiana36 <kelikimardiana@gmail.com>
2024-06-08 22:52:00 +0800

    perbaikan index luar dan views tamu,pemesanan

---
+++ uas/app/views/pemesanan/index.php
@@ -0,0 +1,66 @@
+<div class="content-body">
+
+
+    <!-- row -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Reservation Data</h4>
+                        <div class="table-responsive">
+                            <table class="table table-hover  zero-configuration">
+                                <thead>
+                                    <tr class="gradient-1" style="text-wrap: nowrap">
+                                        <th>Number</th>
+                                        <th>Guest Name</th>
+                                        <th>Reservation Kode</th>
+                                        <th>Room Number</th>
+                                        <th>Check-out</th>
+                                        <th>Check-in</th>
+                                        <th>Status</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <span>
+                                                <a href="index.php?action= &id=" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                    <i class="fa fa-pencil color-muted m-r-5"></i>
+                                                </a>
+                                                <a href="index.php?action= &id=" data-toggle="tooltip" data-placement="top" title="Delete">
+                                                    <i class="fa fa-close color-danger"></i>
+                                                </a>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>Number</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone Number</th>
+                                        <th>Address</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
+<!--**********************************
+        Content body end
+    ***********************************-->
--- uas/index.php
+++ uas/index.php
@@ -6,7 +6,7 @@ require "app/views/components/headers.php";
 require "app/views/components/navbars.php";
 switch ($action) {
     case "rTamu":
-        $_SESSION['page'] = "Tamu";
+        $_SESSION['page'] = "Guest";
         require "app/views/tamu/index.php";
         break;
     case "rUser":
@@ -14,16 +14,70 @@ switch ($action) {
         require "app/views/user/index.php";
         break;
     case "rInfokamar":
-        $_SESSION['page'] = "Kamar";
+        $_SESSION['page'] = "Room";
         require "app/views/infoKamar/index.php";
         break;
     case "rPembayaran":
-        $_SESSION['page'] = "Pembayaran";
+        $_SESSION['page'] = "Payment";
         require "app/views/pembayaran/index.php";
         break;
     case "rPemesanan":
-        $_SESSION['page'] = "Pemesanan";
+        $_SESSION['page'] = "Reservation";
         require "app/views/pemesanan/index.php";
+        break;
+    case "cTamu":
+        $_SESSION['page'] = "Add Guest";
+        require "app/views/tamu/create.php";
+        break;
+    case "cUser":
+        $_SESSION['page'] = "Add User";
+        require "app/views/user/create.php";
+        break;
+    case "cInfokamar":
+        $_SESSION['page'] = "Add Room";
+        require "app/views/infoKamar/create.php";
+        break;
+    case "cPembayaran":
+        $_SESSION['page'] = "Add Payment";
+        require "app/views/pembayaran/create.php";
+        break;
+    case "cPemesanan":
+        $_SESSION['page'] = "Add Reservation";
+        require "app/views/pemesanan/create.php";
+        break;
+    case "uTamu":
+        $_SESSION['page'] = "Edit Guest";
+        require "app/views/tamu/update.php";
+        break;
+    case "uUser":
+        $_SESSION['page'] = "Edit User";
+        require "app/views/user/update.php";
+        break;
+    case "uInfokamar":
+        $_SESSION['page'] = "Edit Room";
+        require "app/views/infoKamar/update.php";
+        break;
+    case "uPembayaran":
+        $_SESSION['page'] = "Edit Payment";
+        require "app/views/pembayaran/update.php";
+        break;
+    case "uPemesanan":
+        $_SESSION['page'] = "Edit Reservation";
+        require "app/views/pemesanan/update.php";
+        break;
+    case "dTamu":
+        break;
+    case "dUser":
+      
+        break;
+    case "dInfokamar":
+      
+        break;
+    case "dPembayaran":
+      
+        break;
+    case "dPemesanan":
+       
         break;
     default:
         echo "Anda salah memasukan action!!";
