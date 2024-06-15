29af8cef69c0d9505683a2d3a9652c2141e05749
triana <agungtrianaputra22@gmail.com>
2024-06-15 15:27:47 +0800

    perbaikan tambah model

--- uas/app/models/kamar.php
+++ uas/app/models/kamar.php
@@ -13,42 +13,6 @@ class infokamar {
         $this->conn = $db;
     }
 
-    public function checkFoto() {
-        $imageUrl = "";
-        if(isset($_FILES['foto']) && $_FILES['foto']['error'] == UPLOAD_ERR_OK) {
-            $targetDir = "uploads/";
-            $targetFile = $targetDir . basename($_FILES["foto"]["name"]);
-            $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-            
-            if ($_FILES["foto"]["size"] > 5000000) {
-                echo "Sorry, your file is too large.";
-                return false;
-            }
-
-            $allowedFileType = ['jpg', 'png', 'jpeg', 'gif'];
-            if (!in_array($imageFileType, $allowedFileType)) {
-                echo "Sorry, your file format is not correct.";
-                return false;
-            }
-
-            if (move_uploaded_file($_FILES["foto"]["tmp_name"], $targetFile)) {
-                $imageUrl = $targetFile;
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-                return false;
-            }
-        } else {
-            if(isset($_FILES['foto']['error']) && $_FILES['foto']['error'] != UPLOAD_ERR_NO_FILE) {
-                echo "Sorry, there was an error with your upload: " . $_FILES['foto']['error'];
-            } else {
-                echo "No file was uploaded.";
-            }
-            return false;
-        }
-        $this->foto = $imageUrl;
-        return true;
-    }
-
     public function readKamar() {
         $query = "SELECT * FROM " . $this->table_name;
         $stmt = $this->conn->prepare($query);
@@ -65,9 +29,7 @@ class infokamar {
     }
 
     public function createKamar() {
-        if(!$this->checkFoto()) {
-            return false;
-        }
+
 
         $query = "INSERT INTO " . $this->table_name . " SET nomor=:nomor, tipe=:tipe, status=:status, harga=:harga, foto=:foto";
         $stmt = $this->conn->prepare($query);
@@ -76,6 +38,7 @@ class infokamar {
         $this->tipe = htmlspecialchars(strip_tags($this->tipe));
         $this->status = htmlspecialchars(strip_tags($this->status));
         $this->harga = htmlspecialchars(strip_tags($this->harga));
+        $this->foto = htmlspecialchars(strip_tags($this->foto));
         
         $stmt->bindParam(":nomor", $this->nomor);
         $stmt->bindParam(":tipe", $this->tipe);
@@ -90,9 +53,6 @@ class infokamar {
     }
 
     public function updateKamar() {
-        if(!$this->checkFoto()) {
-            return false;
-        }
 
         $query = "UPDATE " . $this->table_name . " SET nomor=:nomor, tipe=:tipe, status=:status, harga=:harga, foto=:foto WHERE id=:id";
         $stmt = $this->conn->prepare($query);
@@ -101,8 +61,9 @@ class infokamar {
         $this->tipe = htmlspecialchars(strip_tags($this->tipe));
         $this->status = htmlspecialchars(strip_tags($this->status));
         $this->harga = htmlspecialchars(strip_tags($this->harga));
+        $this->foto = htmlspecialchars(strip_tags($this->foto));
         $this->id = htmlspecialchars(strip_tags($this->id));
-
+        
         $stmt->bindParam(":nomor", $this->nomor);
         $stmt->bindParam(":tipe", $this->tipe);
         $stmt->bindParam(":status", $this->status);
